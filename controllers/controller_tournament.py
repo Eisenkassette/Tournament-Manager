23169edc8b129c260db33bd64b148da2917bd0ef
Daniel <daniel.larionov@protonmail.com>
2024-04-24 16:43:37 +0200

    launch tournament added

--- controllers/controller_tournament.py
+++ controllers/controller_tournament.py
@@ -1,8 +1,10 @@
-# list of rounds here
 from tinydb import TinyDB, Query
 import os
 from datetime import datetime
+from controllers.controller_rounds import RoundsController
 from models.model_tournament import TournamentModel
+
+controller_rounds = RoundsController()
 
 
 class TournamentController:
@@ -21,29 +23,85 @@ class TournamentController:
     def find_tournament_by_name(self, tournament_name):
         return self.table.search(Query().tournament_name == str(tournament_name))
 
+    def find_participants_list_using_name(self, tournament_name):
+        result = self.table.search(Query().tournament_name == str(tournament_name))
+        if result:
+            return result[0]["participants"]
+        else:
+            return None
+
+    def get_date_using_name(self, tournament_name):
+        result = self.table.search(Query().tournament_name == str(tournament_name))
+        if result:
+            return result[0]["start_date"]
+        else:
+            return None
+
     def create_tournament(self,
                           tournament_name: str,
                           location: str,
-                          max_round_number: int,
+                          max_rounds_number: int,
                           description: str,
                           participants: list):
-        tournament = TournamentModel(tournament_name, location, max_round_number, description, -1,
+        tournament = TournamentModel(tournament_name, location, max_rounds_number, description, -1,
                                      -1, 1, [], participants)
         self.table.insert(tournament.to_dict())
         self.tournaments.append(tournament)
 
-    def start_tournament(self, tournament_name: str):
-        current_tournament_doc = self.table.get(Query().tournament_name == str(tournament_name))
-        if current_tournament_doc:
-            current_time = datetime.now().strftime("%d-%m-%Y %H:%M")
-            for tournament in self.tournaments:
-                if tournament.tournament_name == tournament_name:
-                    tournament.start_date = current_time
-                    self.table.update({'start_date': current_time}, Query().tournament_name == tournament_name)
-                    break
+    def set_tournament_start_date(self, tournament_name: str):
+        current_tournament = self.table.get(Query().tournament_name == str(tournament_name))
+        if current_tournament:
+            if current_tournament.get('start_date', -1) == -1:
+                current_time = datetime.now().strftime("%d-%m-%Y %H:%M")
+                self.table.update({'start_date': current_time}, Query().tournament_name == tournament_name)
+                for tournament in self.tournaments:
+                    if tournament.tournament_name == tournament_name:
+                        tournament.start_date = current_time
+                        break
+
+    def get_players_info(self, tournament_name: str):
+        tournament = self.find_tournament_by_name(tournament_name)
+        if tournament:
+            participants = tournament[0]['participants']
+            players_info = []
+            for player_data in participants:
+                for player in player_data:
+                    players_info.append(player)
+            return players_info
         else:
-            print("Tournament not found!")
+            print("Tournament not found.")
+            return None
+
+    def get_max_rounds_number(self, tournament_name: str):
+        tournament = self.find_tournament_by_name(tournament_name)
+        if tournament:
+            return tournament[0]['max_rounds_number']
+        else:
+            print("Tournament not found.")
+            return None
+
+    def launch_tournament(self, tournament_name: str):
+        participants_list = self.get_players_info(tournament_name)
+        max_round_number = int(self.get_max_rounds_number(tournament_name))
+        check = controller_rounds.check_for_match_file(tournament_name)
+        if check == "Not Found":
+            controller_rounds.create_round_one(tournament_name, participants_list)
+        while True:
+            controller_rounds.load_latest_round_from_file(tournament_name)
+            controller_rounds.update_round_start_time(tournament_name,
+                                                      controller_rounds.get_highest_round_number(tournament_name))
+
+            controller_rounds.input_match_results(tournament_name)
+            controller_rounds.update_round_end_time(tournament_name,
+                                                    controller_rounds.get_highest_round_number(tournament_name))
+            controller_rounds.update_latest_match_data(tournament_name)
+            if int(controller_rounds.get_highest_round_number(tournament_name)) < max_round_number:
+                controller_rounds.create_next_round(tournament_name)
+            if int(controller_rounds.get_highest_round_number(tournament_name)) == max_round_number:
+                break
+
 
     def remove_tournament_by_name(self, name: str):
         self.table.remove(Query().tournament_name == str(name))
         self.tournaments = [tournament for tournament in self.tournaments if tournament.tournament_name != str(name)]
+        controller_rounds.delete_round_file(name)
