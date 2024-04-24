1a622eeee154aef6aaf9ad3668bc5c94c1bca467
Daniel <daniel.larionov@protonmail.com>
2024-04-24 16:42:37 +0200

    round creation, match generation created

--- controllers/controller_rounds.py
+++ controllers/controller_rounds.py
@@ -1,6 +1,7 @@
 from tinydb import TinyDB, Query
 from datetime import datetime
 import random
+import json
 import os
 from models.model_rounds import RoundsModel
 
@@ -8,6 +9,7 @@ from models.model_rounds import RoundsModel
 class RoundsController:
     def __init__(self):
         self.matches = []
+        self.participants = []
         self.db_folder = 'data/rounds/'
         if not os.path.exists(self.db_folder):
             os.makedirs(self.db_folder)
@@ -17,19 +19,62 @@ class RoundsController:
         if not os.path.exists(db_file):
             open(db_file, 'w').close()
 
-        db = TinyDB(db_file)
-        table = db.table('rounds')
-        return [RoundsModel(**rounds) for rounds in table.all()]
+        with open(db_file, 'r') as file:
+            data = json.load(file)
+            if "rounds" in data:
+                rounds_data = data["rounds"]
+                round_1_data = rounds_data.get("1", {})  # Assuming you're loading data for round 1
+                if round_1_data:
+                    self.participants = round_1_data.get("participants", [])
+                    self.matches = round_1_data.get("matches", [])
+
+    def get_all_matches(self, tournament_name):
+        db_file = os.path.join(self.db_folder, f"{tournament_name}.rounds.db.json")
+        all_rounds = []
+
+        with open(db_file, 'r') as file:
+            data = json.load(file)
+            round_data = data["rounds"]
+            for round_info in round_data.values():  # Iterate over the values of the rounds dictionary
+                match_data = round_info.get("matches", [])  # Use .get() to safely access matches
+                all_rounds.extend(match_data)  # Extend the all_rounds list with matches
+
+        return all_rounds
+
+    def load_latest_round_from_file(self, tournament_name):
+        db_file = os.path.join(self.db_folder, f"{tournament_name}.rounds.db.json")
+
+        with open(db_file, 'r') as file:
+            data = json.load(file)
+            if "rounds" in data:
+                rounds_data = data["rounds"]
+                latest_round_number = max(map(int, rounds_data.keys()), default=0)  # Get the latest round number
+                latest_round_data = rounds_data.get(str(latest_round_number), {})  # Load data for the latest round
+                if latest_round_data:
+                    self.participants = latest_round_data.get("participants", [])
+                    self.matches = latest_round_data.get("matches", [])
 
     def print_matches(self):
         return self.matches
 
-    def create_round_zero(self, tournament_name: str, participants_list: list):
+    def check_for_match_file(self, tournament_name: str):
+        db_path = os.path.join(self.db_folder, f"{tournament_name}.rounds.db.json")
+        if not os.path.exists(db_path):
+            return "Not Found"
+        else:
+            return "File found"
+
+    def delete_round_file(self, tournament_name):
+        db_file = os.path.join(self.db_folder, f"{tournament_name}.rounds.db.json")
+        if os.path.exists(db_file):
+            os.remove(db_file)
+
+    def create_round_one(self, tournament_name: str, participants_list: list):
         db_path = os.path.join(self.db_folder, f"{tournament_name}.rounds.db.json")
         if not os.path.exists(db_path):
             open(db_path, 'w').close()
-        round_name = "Round 0"
-        start_time = datetime.now().strftime("%d-%m-%Y %H:%M")
+        round_name = "Round 1"
+        start_time = "None"
         for player in participants_list:
             player['score'] = 0
         sorted_players = sorted(participants_list, key=lambda x: x['score'])
@@ -40,9 +85,198 @@ class RoundsController:
                 if i + 1 < len(sorted_players) else None
             self.matches.append([player_1_info, player_2_info])
 
-        round_0 = RoundsModel(start_time, round_name, participants_list, self.matches, start_time)
+        round_1 = RoundsModel(start_time, round_name, participants_list, self.matches, start_time)
 
-        print(self.matches)
         db = TinyDB(db_path)
         table = db.table('rounds')
-        table.insert(round_0.to_dict())
+        table.insert(round_1.to_dict())
+
+    def input_match_results(self, tournament_name):
+        for match in self.matches:
+            player_1_info, player_2_info = match[0], match[1]
+            player_1_name = self.get_player_name(player_1_info[0])
+            player_2_name = self.get_player_name(player_2_info[0])
+
+            print("--------------------------------")
+
+            while True:
+                winner_chess_id = input(f"{player_1_name} {player_1_info[0]} vs {player_2_name} {player_2_info[0]}"
+                                        f"\nEnter winner chessid or type 'tie': ")
+
+                if winner_chess_id.lower() == "tie":
+                    self.update_score(player_1_info[0], 0.5)
+                    self.update_score(player_2_info[0], 0.5)
+                    match[0][1] = self.get_player_score(player_1_info[0])
+                    match[1][1] = self.get_player_score(player_2_info[0])
+                    break
+                elif winner_chess_id == player_1_info[0]:
+                    self.update_score(player_1_info[0], 1)
+                    self.update_score(player_2_info[0], 0)
+                    match[0][1] = self.get_player_score(player_1_info[0])
+                    match[1][1] = self.get_player_score(player_2_info[0])
+                    break
+                elif winner_chess_id == player_2_info[0]:
+                    self.update_score(player_1_info[0], 0)
+                    self.update_score(player_2_info[0], 1)
+                    match[0][1] = self.get_player_score(player_1_info[0])
+                    match[1][1] = self.get_player_score(player_2_info[0])
+                    break
+                else:
+                    print("Wrong input. Please input chessid of winner or 'tie'.")
+
+    def create_next_round(self, tournament_name):
+        db_file = os.path.join(self.db_folder, f"{tournament_name}.rounds.db.json")
+        next_round_number = self.get_highest_round_number(tournament_name) + 1
+
+        # Load existing rounds data
+        with open(db_file, 'r') as file:
+            data = json.load(file)
+            if "rounds" in data:
+                rounds_data = data["rounds"]
+
+                # Update participants list for the new round
+                new_round_data = {
+                    "round_name": f"Round {next_round_number}",
+                    "start_time": "None",
+                    "end_time": "None",
+                    "participants": self.participants,
+                    "matches": self.generate_matches_for_round(tournament_name)
+                }
+
+                # Append the new round data to the existing rounds data
+                rounds_data[str(next_round_number)] = new_round_data
+
+        # Write the updated rounds data back to the file
+        with open(db_file, 'w') as file:
+            json.dump(data, file, indent=4)
+
+    def load_round_matches(self, tournament_name, round_number):
+        db_file = os.path.join(self.db_folder, f"{tournament_name}.rounds.db.json")
+
+        with open(db_file, 'r') as file:
+            data = json.load(file)
+            if "rounds" in data:
+                rounds_data = data["rounds"]
+                round_data = rounds_data.get(str(round_number))
+                if round_data:
+                    return round_data.get("matches", [])
+        return []
+
+    def generate_matches_for_round(self, tournament_name):
+        # Load the data from the latest round
+        self.load_latest_round_from_file(tournament_name)
+
+        # Sort participants based on their scores
+        sorted_participants = sorted(self.participants, key=lambda participant: participant['score'], reverse=True)
+
+        # Get the matches from the latest round
+        latest_round_matches = self.matches
+
+        latest_round_opponents = {}
+        for match in latest_round_matches:
+            for player_info in match:
+                player_id = player_info[0]
+                opponent_id = match[1 - match.index(player_info)][0]
+
+                # Check if player_id is already in the dictionary
+                if player_id not in latest_round_opponents:
+                    # If not, add it with an empty set as the value
+                    latest_round_opponents[player_id] = set()
+
+                # Add opponent_id to the set of opponents for player_id
+                latest_round_opponents[player_id].add(opponent_id)
+
+        # Generate matches based on the sorted list of participants
+        matches = []
+        for i in range(0, len(sorted_participants), 2):
+            player_1_id = sorted_participants[i]['chess_id']
+            player_2_id = sorted_participants[i + 1]['chess_id']
+
+            # Check if player 1 has played against player 2 in the latest round
+            if player_2_id in latest_round_opponents.get(player_1_id, []):
+                # If so, find a new opponent for player 2
+                for j in range(i + 2, len(sorted_participants)):
+                    player_2_id = sorted_participants[j]['chess_id']
+                    if player_2_id not in latest_round_opponents.get(player_1_id, []):
+                        break
+
+            # Add the match to the list of matches
+            matches.append([
+                [player_1_id, sorted_participants[i]['score']],
+                [player_2_id, sorted_participants[i + 1]['score']]
+            ])
+
+        return matches
+
+    def get_player_score(self, chess_id):
+        for participant in self.participants:
+            if participant["chess_id"] == chess_id:
+                return participant["score"]
+        return 0  # Default score if player not found
+
+    def get_highest_round_number(self, tournament_name):
+        db_file = os.path.join(self.db_folder, f"{tournament_name}.rounds.db.json")
+
+        with open(db_file, 'r') as file:
+            data = json.load(file)
+            if "rounds" in data:
+                rounds_data = data["rounds"]
+                if rounds_data:
+                    highest_round_number = max(map(int, rounds_data.keys()))
+                    return highest_round_number
+
+    def get_player_name(self, chess_id):
+        for participant in self.participants:
+            if participant["chess_id"] == chess_id:
+                return f"{participant['first_name']} {participant['last_name']} ({chess_id})"
+        return "Unknown Player"
+
+    def update_latest_match_data(self, tournament_name):
+        db_file = os.path.join(self.db_folder, f"{tournament_name}.rounds.db.json")
+
+        with open(db_file, 'r+') as file:
+            data = json.load(file)
+            if "rounds" in data:
+                rounds_data = data["rounds"]
+                latest_round_number = max(map(int, rounds_data.keys()), default=0)
+                latest_round_data = rounds_data.get(str(latest_round_number), {})
+
+                if latest_round_data:
+                    latest_round_data["participants"] = self.participants
+                    latest_round_data["matches"] = self.matches
+
+                    file.seek(0)
+                    json.dump(data, file, indent=4)
+                    file.truncate()
+
+    def update_round_start_time(self, tournament_name, round_number):
+        db_file = os.path.join(self.db_folder, f"{tournament_name}.rounds.db.json")
+        start_time = datetime.now().strftime("%d-%m-%Y %H:%M")
+
+        with open(db_file, 'r+') as file:
+            data = json.load(file)
+            if "rounds" in data:
+                rounds_data = data["rounds"]
+                if str(round_number) in rounds_data:
+                    rounds_data[str(round_number)]["start_time"] = start_time
+                    file.seek(0)  # Move the file pointer to the beginning
+                    json.dump(data, file, indent=4)  # Write the updated data back to the file
+
+    def update_round_end_time(self, tournament_name, round_number):
+        db_file = os.path.join(self.db_folder, f"{tournament_name}.rounds.db.json")
+        end_time = datetime.now().strftime("%d-%m-%Y %H:%M")
+
+        with open(db_file, 'r+') as file:
+            data = json.load(file)
+            if "rounds" in data:
+                rounds_data = data["rounds"]
+                if str(round_number) in rounds_data:
+                    rounds_data[str(round_number)]["end_time"] = end_time
+                    file.seek(0)  # Move the file pointer to the beginning
+                    json.dump(data, file, indent=4)  # Write the updated data back to the file
+
+    def update_score(self, chess_id, score):
+        for participant in self.participants:
+            if participant["chess_id"] == chess_id:
+                participant["score"] += score
+                break
